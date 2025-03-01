659375f0fee2715cf2c6ba50cd374612b7b26eeb
t1anchen <143927+t1anchen@users.noreply.github.com>
2025-03-01 11:28:54 +0000

    feat: trying adding a separate DateTime class

---
+++ samples/NativeTrainer/src/ntp/DateTime.cpp
@@ -0,0 +1,27 @@
+﻿#include "ntp/DateTime.h"
+#include <numeric>
+
+namespace NTP {
+    
+    DateTime DateTime::operator+(DateTime const& other)
+    {
+        int carry = 0;
+        DateTime result;
+
+        // second
+        auto dv = std::div(second_ + other.second_, 60);
+        carry = dv.quot;
+        result.second_ = dv.rem;
+
+        // minute
+        dv = std::div(minute_ + other.minute_ + carry, 60);
+        carry = dv.quot;
+        result.minute_ = dv.rem;
+
+        // hour
+        dv = std::div(hour_ + other.hour_ + carry, 60);
+        carry = dv.quot;
+        result.hour_ = dv.rem;
+        return result;
+    }
+}
